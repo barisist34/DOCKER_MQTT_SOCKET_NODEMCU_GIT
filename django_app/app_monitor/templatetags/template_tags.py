079d879ea9256be9230316fd5fd944a002f28874
barisist34 <uyarbaris@gmail.com>
2025-10-23 23:10:08 +0300

    251023-- devices_all.html de event=1 yoksa filtreler hata veriyordu. try-except eklendi,kontrol edilecek.

--- django_app/app_monitor/templatetags/template_tags.py
+++ django_app/app_monitor/templatetags/template_tags.py
@@ -25,22 +25,30 @@ def subtract_now_datetime(date):
 
 @register.filter
 def last_event_active(devices,alarm_id):
-    last_event=devices.event_set.filter(alarm_id=alarm_id).last()
-    last_event_status=last_event.event_active
-    if last_event_status == True:
-        print("last_event_active: TRUE")
-        return True
-    else:
-        print("last_event_active: FALSE")
-        return False
+    try:
+        last_event=devices.event_set.filter(alarm_id=alarm_id).last()
+        last_event_status=last_event.event_active
+        if last_event_status == True:
+            print("last_event_active: TRUE")
+            return True
+        else:
+            print("last_event_active: FALSE")
+            return False
+    except Exception as e:
+        print(f"EXCEPTION: last_event_active; {str(e)} ")
+        return "-"
 
 @register.filter
 def last_event_startdate(devices,alarm_id):
-    last_event=devices.event_set.filter(alarm_id=alarm_id).last()
-    last_event_startdate_formatsiz=last_event.start_time
-    # tarih2_formatli=datetime.strftime(tarih2_datetime,'%Y-%m-%d %H:%M') #datetime format değiştirme
-    last_event_startdate=datetime.strftime(last_event_startdate_formatsiz,'%d-%m-%Y / %H:%M:%S')
-    return last_event_startdate
+    try:
+        last_event=devices.event_set.filter(alarm_id=alarm_id).last()
+        last_event_startdate_formatsiz=last_event.start_time
+        # tarih2_formatli=datetime.strftime(tarih2_datetime,'%Y-%m-%d %H:%M') #datetime format değiştirme
+        last_event_startdate=datetime.strftime(last_event_startdate_formatsiz,'%d-%m-%Y / %H:%M:%S')
+        return last_event_startdate
+    except Exception as e:
+        print(f"EXCEPTION: last_event_startdate; {str(e)} ")
+        return "-"
 
 @register.filter
 def last_event_finishdate(devices,alarm_id):
@@ -53,41 +61,53 @@ def last_event_finishdate(devices,alarm_id):
         print("datetime.strftime(last_event_finishtdate_formatsiz,'%d-%m-%Y %H:%M:%S'): {last_event_finishdate}")
         return last_event_finishdate
     except Exception as e:
-        print(f"Exception var,finishdate yok,cihaz çalışıyor: {str(e)}")
+        print(f"EXCEPTION: last_event_finishdate; {str(e)} ")
         return "-"
 
 @register.filter
 def last_event_duration(devices,alarm_id):
-    last_event=devices.event_set.filter(alarm_id=alarm_id).last()
-    last_event_duration=last_event.event_duration
-    last_event_startdate_formatsiz=last_event.start_time
-    # tarih2_formatli=datetime.strftime(tarih2_datetime,'%Y-%m-%d %H:%M') #datetime format değiştirme
-    last_event_startdate=datetime.strftime(last_event_startdate_formatsiz,'%d-%m-%Y %H:%M')
-    return last_event_startdate
+    try:
+        last_event=devices.event_set.filter(alarm_id=alarm_id).last()
+        last_event_duration=last_event.event_duration
+        last_event_startdate_formatsiz=last_event.start_time
+        # tarih2_formatli=datetime.strftime(tarih2_datetime,'%Y-%m-%d %H:%M') #datetime format değiştirme
+        last_event_startdate=datetime.strftime(last_event_startdate_formatsiz,'%d-%m-%Y %H:%M')
+        return last_event_startdate
+    except Exception as e:
+        print(f"EXCEPTION: last_event_duration; {str(e)} ")
+        return "-"
 
 @register.filter
 def active_device_duration(devices,alarm_id):
-    last_event=devices.event_set.filter(alarm_id=alarm_id).last()
-    print(f"devices.event_set.filter(alarm_id=alarm_id).last(): {devices.event_set.filter(alarm_id=alarm_id).last()}")
-    active_device_duration_formatsiz=datetime.now() - last_event.finish_time
-    print(f"datetime.now() - last_event.finish_time: {datetime.now() - last_event.finish_time}")
-    last_event_startdate_formatsiz=last_event.start_time
-    # tarih2_formatli=datetime.strftime(tarih2_datetime,'%Y-%m-%d %H:%M') #datetime format değiştirme
-    # active_device_duration=datetime.strftime(active_device_duration_formatsiz,'%Y-%m-%d %H:%M')
-    # return active_device_duration_formatsiz
-    return str(active_device_duration_formatsiz).split(".")[0]
+    try:
+        last_event=devices.event_set.filter(alarm_id=alarm_id).last()
+        print(f"devices.event_set.filter(alarm_id=alarm_id).last(): {devices.event_set.filter(alarm_id=alarm_id).last()}")
+        active_device_duration_formatsiz=datetime.now() - last_event.finish_time
+        print(f"datetime.now() - last_event.finish_time: {datetime.now() - last_event.finish_time}")
+        last_event_startdate_formatsiz=last_event.start_time
+        # tarih2_formatli=datetime.strftime(tarih2_datetime,'%Y-%m-%d %H:%M') #datetime format değiştirme
+        # active_device_duration=datetime.strftime(active_device_duration_formatsiz,'%Y-%m-%d %H:%M')
+        # return active_device_duration_formatsiz
+        return str(active_device_duration_formatsiz).split(".")[0]
+    except Exception as e:
+        print(f"EXCEPTION: active_device_duration; {str(e)} ")
+        return "-"
 
 @register.filter
 def outage_device_duration(devices,alarm_id):
-    last_event=devices.event_set.filter(alarm_id=alarm_id).last()
-    print(f"devices.event_set.filter(alarm_id=alarm_id).last(): {devices.event_set.filter(alarm_id=alarm_id).last()}")
-    outage_device_duration_formatsiz=datetime.now() - last_event.start_time
-    print(f"datetime.now() - last_event.finish_time: {datetime.now() - last_event.start_time}")
-    last_event_startdate_formatsiz=last_event.start_time
-    # tarih2_formatli=datetime.strftime(tarih2_datetime,'%Y-%m-%d %H:%M') #datetime format değiştirme
-    # active_device_duration=datetime.strftime(active_device_duration_formatsiz,'%Y-%m-%d %H:%M')
-    # return outage_device_duration_formatsiz
-    return str(outage_device_duration_formatsiz).split(".")[0]
+    try:
+        last_event=devices.event_set.filter(alarm_id=alarm_id).last()
+        print(f"devices.event_set.filter(alarm_id=alarm_id).last(): {devices.event_set.filter(alarm_id=alarm_id).last()}")
+        outage_device_duration_formatsiz=datetime.now() - last_event.start_time
+        print(f"datetime.now() - last_event.finish_time: {datetime.now() - last_event.start_time}")
+        last_event_startdate_formatsiz=last_event.start_time
+        # tarih2_formatli=datetime.strftime(tarih2_datetime,'%Y-%m-%d %H:%M') #datetime format değiştirme
+        # active_device_duration=datetime.strftime(active_device_duration_formatsiz,'%Y-%m-%d %H:%M')
+        # return outage_device_duration_formatsiz
+        return str(outage_device_duration_formatsiz).split(".")[0]
+    except Exception as e:
+        print(f"EXCEPTION: active_device_duration; {str(e)} ")
+        return "-"
 
 # @register.filter
 # def Takimlar_filter(queryset,filter):
